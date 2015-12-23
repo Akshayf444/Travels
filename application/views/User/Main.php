24e2d43a8509cc2512bece12ee9fdb29acb21bc9
raiyan2723 <raiyanshaikh1993@gmail.com>
2015-12-23 16:04:17 +0530

    added page detail

--- application/views/User/Main.php
+++ application/views/User/Main.php
@@ -14,6 +14,42 @@
     <div class="item"><img src="<?php echo asset_url() ?>images/2.jpg" alt="GTA V"></div>
     <div class="item"><img src="<?php echo asset_url() ?>images/3.jpg" alt="Mirror Edge"></div>
 </div>
+
+<ul class="table-view">
+    <li class="table-view-divider">POPULAR DESTINATIONS</li>
+    <li class="table-view-cell">
+            <li class="table-view-cell media">
+                <a class="navigate-right">
+                    <img class="media-object pull-left" src="<?php echo asset_url() ?>images/Venice.jpg">
+                    <div class="media-body">
+                        Venice, Italy
+                        <p>Venice is built on 117 small islands and has some 150 canals and 409 bridges.</p>
+                        <button class="btn btn-primary btn-block">Book Now</button>
+                    </div>
+                </a>
+            </li>
+            <li class="table-view-cell media">
+                <a class="navigate-right">
+                    <img class="media-object pull-left" src="http://placehold.it/42x42">
+                    <div class="media-body">
+                        Item 1
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore. Lorem ipsum dolor sit amet.</p>
+                    </div>
+                </a>
+            </li>
+            <li class="table-view-cell media">
+                <a class="navigate-right">
+                    <img class="media-object pull-left" src="http://placehold.it/42x42">
+                    <div class="media-body">
+                        Item 1
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore. Lorem ipsum dolor sit amet.</p>
+                    </div>
+                </a>
+            </li>
+        
+    </li>
+</ul>
+
 <script src="<?php echo asset_url() ?>js/owl.carousel.min.js" type="text/javascript"></script>
 <script>
     $(document).ready(function () {
