ab2ed2b4b03a4a45ba9f179f3b25a364d6d1cdbe
Akshayf444 <mohite.akshay118@gmail.com>
2015-12-23 12:25:07 +0530

    Added Search result

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -7,15 +7,16 @@ class User extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
-        
     }
 
     public function index() {
-        
         $data = array('title' => 'Main', 'content' => 'User/Main', 'view_data' => 'blank');
         $this->load->view('template2', $data);
     }
-
-   
+    
+    public function SearchResult(){
+        $data = array('title' => 'Main', 'content' => 'User/SearchResult', 'view_data' => 'blank');
+        $this->load->view('template2', $data);
+    }
 
 }
--- application/views/template2.php
+++ application/views/template2.php
@@ -1,12 +1,12 @@
 <html lang="en">
 
-        <link href="<?php echo asset_url() ?>css/ratchet-theme-ios.min.css" rel="stylesheet" type="text/css"/>
-        <script src="<?php echo asset_url() ?>js/ratchet.min.js" type="text/javascript"></script>
-       <div align="center">
-           <header style="    height: 10%;" class="bar bar-nav">
-        <h1 class="title" >
-            <img style="    height: 100%;" src="<?php echo asset_url() ?>images/travels.png" alt=""/>
-        </h1>
-    </header>
-</div>
+    <link href="<?php echo asset_url() ?>css/ratchet-theme-ios.min.css" rel="stylesheet" type="text/css"/>
+    <script src="<?php echo asset_url() ?>js/ratchet.min.js" type="text/javascript"></script>
+    <div align="center">
+        <header style="    height: 10%;" class="bar bar-nav">
+            <h1 class="title" >
+                <img style="    height: 100%;" src="<?php echo asset_url() ?>images/travels.png" alt=""/>
+            </h1>
+        </header>
+    </div>
 </html>
