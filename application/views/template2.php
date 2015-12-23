a0c31b2203ebcb9bbc49d0432b7ca3a7e71cb12e
raiyan2723 <raiyanshaikh1993@gmail.com>
2015-12-23 12:22:10 +0530

    Added main page

--- application/views/template2.php
+++ application/views/template2.php
@@ -1,12 +1,26 @@
 <html lang="en">
 
-        <link href="<?php echo asset_url() ?>css/ratchet-theme-ios.min.css" rel="stylesheet" type="text/css"/>
-        <script src="<?php echo asset_url() ?>js/ratchet.min.js" type="text/javascript"></script>
-       <div align="center">
-           <header style="    height: 10%;" class="bar bar-nav">
-        <h1 class="title" >
-            <img style="    height: 100%;" src="<?php echo asset_url() ?>images/travels.png" alt=""/>
-        </h1>
-    </header>
-</div>
+    <link href="<?php echo asset_url() ?>css/ratchet-theme-ios.min.css" rel="stylesheet" type="text/css"/>
+    <link href="<?php echo asset_url() ?>css/app.css" rel="stylesheet" type="text/css"/>
+    <script src="<?php echo asset_url() ?>js/ratchet.min.js" type="text/javascript"></script>
+    <script src="<?php echo asset_url() ?>js/modals.js" type="text/javascript"></script>
+    <script src="<?php echo asset_url() ?>js/popovers.js" type="text/javascript"></script>
+    <script src="<?php echo asset_url() ?>js/push.js" type="text/javascript"></script>
+    <script src="<?php echo asset_url() ?>js/sliders.js" type="text/javascript"></script>
+    <script src="<?php echo asset_url() ?>js/toggles.js" type="text/javascript"></script>
+    <script src="<?php echo asset_url() ?>js/segmented-controllers.js" type="text/javascript"></script>
+    <link href="<?php echo asset_url() ?>css/docs.css" rel="stylesheet" type="text/css"/>
+    <link href="<?php echo asset_url() ?>css/docs.min.css" rel="stylesheet" type="text/css"/>
+    <link href="<?php echo asset_url() ?>css/docs.css" rel="stylesheet" type="text/css"/>
+    <script src=<?php echo asset_url() ?>js/docs.js" type="text/javascript"></script>
+    <script src="<?php echo asset_url() ?>js/docs.min.js" type="text/javascript"></script>
+    <script src="<?php echo asset_url() ?>js/fingerblast.js" type="text/javascript"></script>
+    <div align="center">
+        <header style="    height: 10%;" class="bar bar-nav">
+            <h1 class="title" >
+                <img style="    height: 100%;" src="<?php echo asset_url() ?>images/travels.png" alt=""/>
+            </h1>
+        </header>
+    </div>
+     <?php $this->load->view('footer'); ?>
 </html>
