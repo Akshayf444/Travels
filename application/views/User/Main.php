d39b3ff7c12e91a4d9e8bcab9a502687458c4e40
raiyan2723 <raiyanshaikh1993@gmail.com>
2015-12-23 16:47:03 +0530

    added detail in slider 2

--- application/views/User/Main.php
+++ application/views/User/Main.php
@@ -53,11 +53,62 @@
         <div class="panel panel-default">
             <div class="panel-body">
                 <div class="col-xs-6">
-                    <img class="img-responsive" src="<?php echo asset_url() ?>images/Venice.jpg">
+                    <img class="img-responsive" src="<?php echo asset_url() ?>images/Paris.jpg">
                 </div>
                 <div class="col-xs-6">
-                     <h4>Venice</h4>
-                    <p>Venice is built on 117 small islands and has some 150 canals and 409 bridges.</p>
+                     <h4>Paris, France</h4>
+                    <p>The most beautiful and romantic of all cities in the world.</p>
+                </div>
+            </div>
+            <div class="panel-footer">
+                <button class="btn btn-primary">Book Now</button>
+            </div>
+        </div>
+
+    </div>
+    <div class="item"> 
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <div class="col-xs-6">
+                    <img class="img-responsive" src="<?php echo asset_url() ?>images/Orlando.jpg">
+                </div>
+                <div class="col-xs-6">
+                     <h4>Orlando, USA</h4>
+                    <p>Orlando is the family entertainment capital of the world.</p>
+                </div>
+            </div>
+            <div class="panel-footer">
+                <button class="btn btn-primary">Book Now</button>
+            </div>
+        </div>
+
+    </div>
+    <div class="item"> 
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <div class="col-xs-6">
+                    <img class="img-responsive" src="<?php echo asset_url() ?>images/Dubai.jpg">
+                </div>
+                <div class="col-xs-6">
+                     <h4>Dubai, UAE</h4>
+                    <p>Dubai makes for great short break in the sun.</p>
+                </div>
+            </div>
+            <div class="panel-footer">
+                <button class="btn btn-primary">Book Now</button>
+            </div>
+        </div>
+
+    </div>
+    <div class="item"> 
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <div class="col-xs-6">
+                    <img class="img-responsive" src="<?php echo asset_url() ?>images/Cape_Town.jpg">
+                </div>
+                <div class="col-xs-6">
+                     <h4>Cape Town</h4>
+                    <p>The most popular international tourist destination in South Africa.</p>
                 </div>
             </div>
             <div class="panel-footer">
