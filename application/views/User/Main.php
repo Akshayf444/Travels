ace70725f12cb57f83d11b1d7eeac402835cfebd
raiyan2723 <raiyanshaikh1993@gmail.com>
2015-12-23 17:37:23 +0530

    linked footer

--- application/views/User/Main.php
+++ application/views/User/Main.php
@@ -126,6 +126,7 @@
         $("#owl-demo").owlCarousel({
             navigation: true, // Show next and prev buttons
             slideSpeed: 300,
+            autoPlay:3000,
             paginationSpeed: 400,
             singleItem: true,
             navigation : false
