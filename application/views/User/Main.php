d17d1b67216ce2f37f647b7389dde5d266297616
Akshayf444 <mohite.akshay118@gmail.com>
2015-12-23 15:39:34 +0530

    Added carousal

--- application/views/User/Main.php
+++ application/views/User/Main.php
@@ -1,17 +1,27 @@
-<div class="slider" id="mySlider">
-  <div class="slide-group">
-    <div class="slide">
-      <img src="/assets/img/slide-1.jpg">
-      <span class="slide-text">
-        <span class="icon icon-left-nav"></span>
-        Slide me
-      </span>
-    </div>
-    <div class="slide">
-      <img src="/assets/img/slide-2.jpg">
-    </div>
-    <div class="slide">
-      <img src="/assets/img/slide-3.jpg">
-    </div>
-  </div>
+<link href="<?php echo asset_url() ?>css/owl.carousel.css" rel="stylesheet" type="text/css"/>
+<link href="<?php echo asset_url() ?>css/owl.transitions.css" rel="stylesheet" type="text/css"/>
+<link href="<?php echo asset_url() ?>css/responsive.css" rel="stylesheet" type="text/css"/>
+<style>
+    #owl-demo .item img{
+        display: block;
+        width: 100%;
+        height: auto;
+    }
+</style>
+<div id="owl-demo" class="owl-carousel owl-theme">
+    <div class="item"><img src="<?php echo asset_url() ?>images/2.jpg" alt="The Last of us"></div>
+    <div class="item"><img src="<?php echo asset_url() ?>images/2.jpg" alt="GTA V"></div>
+    <div class="item"><img src="<?php echo asset_url() ?>images/3.jpg" alt="Mirror Edge"></div>
 </div>
+<script src="<?php echo asset_url() ?>js/owl.carousel.min.js" type="text/javascript"></script>
+<script>
+    $(document).ready(function () {
+        $("#owl-demo").owlCarousel({
+            navigation: true, // Show next and prev buttons
+            slideSpeed: 300,
+            paginationSpeed: 400,
+            singleItem: true,
+            navigation : false
+        });
+    });
+</script>
