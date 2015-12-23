8cfc186fee896439d6dc0de498546f0b2e0d5785
Akshayf444 <mohite.akshay118@gmail.com>
2015-12-23 12:56:39 +0530

    Changes in layout

--- application/views/template2.php
+++ application/views/template2.php
@@ -6,18 +6,19 @@
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="apple-mobile-web-app-status-bar-style" content="black">
         <link href="<?php echo asset_url() ?>css/ratchet.css" rel="stylesheet" type="text/css"/>
-        <link href="<?php echo asset_url() ?>css/app.css" rel="stylesheet" type="text/css"/>
         <link href="<?php echo asset_url() ?>css/ratchet-theme-ios.min.css" rel="stylesheet" type="text/css"/>
-        <link href="<?php echo asset_url() ?>css/docs.min.css" rel="stylesheet" type="text/css"/>
+        <link href="<?php echo asset_url() ?>css/app.css" rel="stylesheet" type="text/css"/>
         <link href="<?php echo asset_url() ?>css/pygments-manni.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
-        <header style="height: 11%;" class="bar bar-nav">
+        <header class="bar bar-nav">
             <h1 class="title" >
-                <img style="height: 99%;" src="<?php echo asset_url() ?>images/travels.png" alt=""/>
+                <img style="height: 99%" src="<?php echo asset_url() ?>images/travels.png" alt=""/>
             </h1>
         </header>
-        <?php $this->load->view($content, $view_data); ?>
+        <div class="content">
+            <?php $this->load->view($content, $view_data); ?>
+        </div>
         <?php $this->load->view('footer'); ?>
         <script src="<?php echo asset_url() ?>js/jquery.js" type="text/javascript"></script>
         <script src="<?php echo asset_url() ?>js/ratchet.min.js" type="text/javascript"></script>
